b3a4897a89170400f0f301cbb66e03112f97fc90
ayanassar2011-tech <aya.nassar2011@gmail.com>
2026-01-05 14:01:24 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
-
+!wget https://raw.githubusercontent.com/yotam-biu/ps9/main/parkinsons.csv -O /content/parkinsons.csv
+!wget https://raw.githubusercontent.com/yotam-biu/python_utils/main/lab_setup_do_not_edit.py -O /content/lab_setup_do_not_edit.py
+import lab_setup_do_not_edit
+import pandas as pd 
+df = pd.read_csv("/content/parkinsons.csv")
+df = df.dropna()
+df.head()
+print(df.columns.to_list())
+X = df[["MDVP:Fo(Hz)", "MDVP:Jitter(%)"]]
+y = df["status"]
+from sklearn.preprocessing import MinMaxScaler
+scaler = MinMaxScaler() 
+X_scaled = scaler.fit_transform(X)
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+from sklearn.neighbors import KNeighborsClassifier
+model = KNeighborsClassifier(n_neighbors=1)
+model.fit(X_train, y_train)
+from sklearn.metrics import accuracy_score
+y_pred = model.predict(X_test)
+accuracy = accuracy_score(y_test, y_pred)
+print(f'Accuracy: {accuracy}')
+import joblib
+joblib.dump(model, 'model.joblib')
